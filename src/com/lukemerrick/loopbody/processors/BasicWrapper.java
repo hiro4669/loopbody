be5663ce63c2f946d638a67d0f395f9250e02e2b
Luke <luke@virtualbox>
2016-07-25 17:24:39 +0900

    added a lot of code to the processor, even though I can't run it

--- src/com/lukemerrick/loopbody/processors/BasicWrapper.java
+++ src/com/lukemerrick/loopbody/processors/BasicWrapper.java
@@ -1,5 +1,9 @@
 package com.lukemerrick.loopbody.processors;
 
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.ArrayList;
+
 // TODO: use correct import statements
 // import spoon.processing.AbstractProcessor;
 // import spoon.reflect.code.CtCodeSnippetStatement;
@@ -21,10 +25,40 @@ public class BasicWrapper extends AbstractProcessor<CtLoop> {
 
 	// Step 1: pick a loop
 	public void process(CtLoop element) {
+
 		// TODO Step 2: identify if a return statment exists
-		// TODO Step 3: identify all local variables accessed within the loop
+		List<CtReturn> retStatementList = element.getBody().getElements(new TypeFilter<CtReturn>());
+		boolean hasReturnStatement = retStatementList.isEmpty();
+
+
+		// Step 3: identify all local variables accessed within the loop
+		// TODO: make sure this works with objects and arrays
+		Filter localVarAccessFilter = new TypeFilter(CtVariableAccess.class);
+		ArrayList<CtVariableAccess> localVarAccesses = element.getElements(localVarAccessFilter);
+		HashSet<CtVariableReference> referencedVars = new HashSet<CtVariableReference>();
+		for (a : localVarAccesses) {
+			referencedVars.add(a.getVariable());
+		}
+
+
+
 		// TODO Step 4: Create internal class
 		CtClass envClass = factory.Core().createClass();
+		HashMap<CtLocalVariableReference> variableMappings = new HashMap<CtLocalVariableReference>();
+		for (CtLocalVariableReference var : referencedVars) {
+			// create "cache variable" and add it to this new class
+			// map the caching inside of "variableMappings"
+		}
+
+		
+		CtElement loopBodyMethod = getFactory().Core().createMethod();
+		LoopBodyMethod.setType(Boolean.class);
+		if (hasReturnStatement) { //set up a local value to store the return value
+			CtExpression retExpression = retStatementList[0].getReturnedExpression();
+			CtLocalVariable retVar = getFactory().Core().createLocalVariable();
+			retVar.setType(retExpression.getType());
+		}
+		
 			/* ---- Inner class specifications: -----
 			* -> class-local version of every local variable used in the loop body
 			* -> class-local variable of return type of most abstract-typed return statement
